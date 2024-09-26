44971f63a0c0a61bf056e0db5036ce99e0d3bac0
LukasProgramacion <lukas.arts315@gmail.com>
2024-09-26 21:19:54 +0200

    BolaJuego(26/09/2024)

---
+++ Assets/Scripts/Ejemplo Vectores.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploVectores : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        transform.position = new Vector3(3, 5, 0);
+        transform.eulerAngles = new Vector3(30, 45, 90);
+        transform.localScale = new Vector3(2, 4, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 
+    
+}
