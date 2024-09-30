83828546bb8bd2f7b079d60526ac3ee0d088bc3a
LukasProgramacion <lukas.arts315@gmail.com>
2024-09-30 17:09:13 +0200

    Avance 30/09/24

---
+++ Assets/Scripts/Ejemplo Vectores 2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploVectores2 : MonoBehaviour
+{
+    [SerializeField] Vector3 direccionGiro = new Vector3(0, 5, 0), direccion = new Vector3(0, 0.5f, 0);
+    [SerializeField] float velocidadGiro = 7, velocidad = 50000.0f;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 1 * Time.deltaTime;
+        if (timer < 0.1f)
+        {
+            transform.Rotate(direccionGiro * velocidadGiro * Time.deltaTime);
+            transform.Translate(direccion.normalized * velocidad * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            direccion = direccion * -1;
+            timer = 0;
+        }
+    }
+        
+}
--- Assets/Scripts/Ejemplo Vectores.cs
+++ Assets/Scripts/Ejemplo Vectores.cs
@@ -5,20 +5,35 @@ using UnityEngine;
 public class EjemploVectores : MonoBehaviour
 {
 
-    
+
+    [SerializeField] Vector3 direccion = new Vector3(2, 0, 0);
+    [SerializeField] int velocidad = 5;
+    float timer;
     // Start is called before the first frame update
     void Start()
     {
         
 
-        transform.position = new Vector3(3, 5, 0);
-        transform.eulerAngles = new Vector3(30, 45, 90);
-        transform.localScale = new Vector3(2, 4, 5);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += 1 * Time.deltaTime;
+
+        if (timer > 0 && timer < 5f)
+        {
+            transform.Translate(direccion.normalized * velocidad * Time.deltaTime);
+        }
+        else
+        {
+            direccion = direccion * -1f;
+            timer = 0f;
+        }
+       
+        
+        
         
     }
 
