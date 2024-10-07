bf6cdf0ddf487e020466ae57c5499b5a7c4799c7
LukasProgramacion <lukas.arts315@gmail.com>
2024-10-07 17:13:04 +0200

    Avance bola 1 (7/10/24)

--- Assets/Scripts/Bola.cs
+++ Assets/Scripts/Bola.cs
@@ -7,6 +7,9 @@ public class Bola : MonoBehaviour
     [SerializeField] Vector3 direccion = new Vector3 (0, 0, 0);
     [SerializeField] float velocidad;
     [SerializeField] float fuerzaSalto, fuerzaX;
+    [SerializeField] int vida;
+    private float h;
+    private float v; 
 
     Rigidbody rb;
 
@@ -19,24 +22,49 @@ public class Bola : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal"); // h = 1 (D ó ->), h = -1 (A ó <-), h = 0 (NADA) 
-        float v = Input.GetAxisRaw("Vertical"); // v = 1 (W ó ^), v = -1 (S ó v), v = 0 (NADA) 
 
-        
-            //direccion = new Vector3(h, 0, v);
+        h = Input.GetAxisRaw("Horizontal"); // h = 1 (D ó ->), h = -1 (A ó <-), h = 0 (NADA) 
 
-           // ESTO ES MOVIMIENTO CON KINEMATIC NO RESPETA FISICA SE JODEN COSAS
-           //transform.position += new Vector3(h, 0, v) * velocidad * Time.deltaTime;
-        
-        
-        
-        
+        v = Input.GetAxisRaw("Vertical"); // v = 1 (W ó ^), v = -1 (S ó v), v = 0 (NADA) 
+                                        //direccion = new Vector3(h, 0, v);
+
+        // ESTO ES MOVIMIENTO CON KINEMATIC NO RESPETA FISICA SE JODEN COSAS
+        //transform.position += new Vector3(h, 0, v) * velocidad * Time.deltaTime;
+
+
+
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(0, fuerzaSalto, 0, ForceMode.Impulse);
         }
         
-        rb.AddForce(h, 0,v , ForceMode.Force);
+        
+        
+    }
+        
+    private void FixedUpdate()
+    {
+        rb.AddForce(h, 0, v, ForceMode.Force);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Colecionable")
+        {
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Trampa"))
+        {
+            vida -= 10;
+            if (vida <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
         
     }
 }
